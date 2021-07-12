ef54fea4b52aba2dd127c0287868e595d5170742
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-13 01:09:44 +0900

    2021-07-13_01

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ made by HyungJin Bang
 
 import os
 import sys
+import re
+from functools import partial
+
+from PyQt5 import Qt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QDateTime, QTime
+from PyQt5.QtCore import *
 from PyQt5.QtGui import QFontDatabase, QFont
 import configparser
 from qt_material import apply_stylesheet
@@ -21,6 +25,12 @@ def open_directory():  # 저장 공간 열기
     if not os.path.isdir(path):
         os.mkdir(path)
     os.startfile(config_parser['directory']['directory'])
+
+
+def open_browser():  # 웹 브라우저 열기
+    url = 'https://www.youtube.com/'
+    webbrowser.open(url)
+
 
 
 
@@ -56,8 +66,8 @@ class YoutubeDownloader(QWidget):
             config_parser.set('directory', 'directory', 'Videos')
             config_parser.write(configfile)
         configfile.close()
-        self.storage_label.setText("현재 저장 장소 = " + config_parser['directory']['directory'])
-        self.storage_label.repaint()
+        self.directory_label.setText("현재 저장 장소 = " + config_parser['directory']['directory'])
+        self.directory_label.repaint()
 
     def select_directory(self):  # 저장 공간 설정
         config_parser = configparser.ConfigParser()
@@ -66,16 +76,64 @@ class YoutubeDownloader(QWidget):
             config_parser.set('directory', 'directory', QFileDialog.getExistingDirectory(self, "select Directory"))
             config_parser.write(configfile)
         configfile.close()
-        self.storage_label.setText("현재 저장 장소 = " + config_parser['directory']['directory'])
-        self.storage_label.repaint()
+        self.directory_label.setText("현재 저장 장소 = " + config_parser['directory']['directory'])
+        self.directory_label.repaint()
 
-    def open_browser(self):  # 웹 브라우저 열기
-        url = 'https://www.youtube.com/'
-        webbrowser.open(url)
+    def save_video(self):  # 동영상 저장
+        url = self.input_url.text()
+        try:
+            print(url)
+            self.youtube = YouTube(url)
+            youtube_streams = self.youtube.streams
+            video_vertical = QVBoxLayout()
+            video_vertical.addStretch(1)
+            video_name = QLabel("비디오 이름")
+            video_name.setFont(QFont("궁서", 15))
+            video_name.setStyleSheet("Color: #6495ED")
+            video_vertical.addWidget(video_name)
+            video_vertical.addStretch(1)
+            video_title = QLabel(f'- {self.youtube.title}')
+            video_title.setFont(QFont("궁서", 15))
+            video_title.setStyleSheet("Color: #6495ED")
+            video_vertical.addWidget(video_title)
+            video_vertical.addStretch(1)
+            video_len = QLabel(f'비디오 길이\n - {self.youtube.length // 3600} 시간 {(self.youtube.length % 3600) // 60} 분 {self.youtube.length % 60} 초')
+            print(type(self.youtube.length))
+            video_len.setFont(QFont("궁서", 15))
+            video_len.setStyleSheet("Color: #6495ED")
+            video_vertical.addWidget(video_len)
+            video_vertical.addStretch(1)
+
+            for i, stream in enumerate(youtube_streams.filter(adaptive=True, file_extension="mp4", mime_type="video/mp4")):
+                print(i, stream, type(stream), stream.resolution)
+                video_horizontal = QHBoxLayout()
+                input_save_btn = QPushButton(f'해상도 = {stream.resolution}\tfps = {stream.fps}')
+                input_save_btn.clicked.connect(partial(self.save_btn_clicked,youtube_streams , stream.itag))
+                video_horizontal.addWidget(input_save_btn)
+                video_vertical.addLayout(video_horizontal)
+                video_vertical.addStretch(1)
+            # video_horizontal.addLayout(video_vertical)
+            self.save_dialog.setWindowTitle("Available formats")
+            self.save_dialog.setWindowModality(Qt.ApplicationModal)
+            self.save_dialog.setLayout(video_vertical)
+            self.save_dialog.show()
+        except:
+            print("error")
+
+    def save_btn_clicked(self, input_stream, itag):
+        stream = input_stream.get_by_itag(itag)
+        config_parser = configparser.ConfigParser()
+        config_parser.read('config.ini', encoding='utf-8')
+        stream.download(output_path=config_parser['directory']['directory'])
+        print("done")
+        self.youtube = None
+        self.save_dialog.close()
 
     def __init__(self):
         super().__init__()
 
+        self.save_dialog = QDialog()
+        self.input_url = QLineEdit(self)
         self.datetime = QDateTime.currentDateTime()
         self.init_ui()
 
@@ -92,61 +150,82 @@ class YoutubeDownloader(QWidget):
         reset_str_btn.setToolTip('저장 장소를 초기화 합니다.')
         reset_str_btn.clicked.connect(self.reset_str)  # 버튼이 클릭되면 해당 함수 실행
 
-        save_btn = QPushButton('저장 장소 설정')
-        save_btn.setToolTip('스크린샷이 저장될 위치를 선택합니다.')
-        save_btn.clicked.connect(self.select_directory)  # 버튼이 클릭되면 해당 함수 실행
+        set_save_btn = QPushButton('저장 장소 설정')
+        set_save_btn.setToolTip('스크린샷이 저장될 위치를 선택합니다.')
+        set_save_btn.clicked.connect(self.select_directory)  # 버튼이 클릭되면 해당 함수 실행
 
         open_btn = QPushButton('저장 장소 열기')
         open_btn.setToolTip('스크린샷이 저장된 위치를 엽니다.')
         open_btn.clicked.connect(open_directory)  # 버튼이 클릭되면 해당 함수 실행
 
-        browser_btn = QPushButton('웹 브라우저 열기')
+        browser_btn = QPushButton('YouTube 열기')
         browser_btn.setToolTip('웹 브라우저를 엽니다.')
-        browser_btn.clicked.connect(self.open_browser)  # 버튼이 클릭되면 해당 함수 실행
+        browser_btn.clicked.connect(open_browser)  # 버튼이 클릭되면 해당 함수 실행
+
+        save_btn = QPushButton('저장')
+        save_btn.setToolTip('해당 동영상을 저장합니다')
+        save_btn.clicked.connect(self.save_video)  # 버튼이 클릭되면 해당 함수 실행
+
+        # 라벨 생성
+        date_label = QLabel('오늘 날짜 : ' + self.datetime.toString('yyyy 년 MM 월 dd 일'))
+        date_label.setFont(QFont("", 20))
+        date_label.setStyleSheet("Color: #6495ED")
+
+        manual_label = QLabel("사용법 : 저장하고 싶은 YouTube 동영상의 URL을 입력하여 주세요")
+        manual_label.setFont(QFont("", 20))
+        manual_label.setStyleSheet("Color: #6495ED")
+
+        self.directory_label = QLabel(storage_text, self)
+        self.directory_label.setFont(QFont("", 20))
+        self.directory_label.setStyleSheet("Color: #6495ED")
 
         # 박스 레이아웃 생성
         box_1 = QHBoxLayout()
         box_1.addStretch(1)
-        date_label = QLabel('오늘 날짜 : ' + self.datetime.toString('yyyy 년 MM 월 dd 일'))
-        date_label.setFont(QFont("", 20))
+
         box_1.addWidget(date_label)
         box_1.addStretch(1)
 
         box_2 = QHBoxLayout()
         box_2.addStretch(1)
-        directory_label = QLabel(storage_text, self)
-        directory_label.setFont(QFont("", 20))
-        box_2.addWidget(directory_label)
+
+        box_2.addWidget(self.directory_label)
         box_2.addStretch(1)
 
         box_3 = QHBoxLayout()
-        box_3.addWidget(QLabel("URL을 입력해 주세요."))
-        input_url = QLineEdit()
-        box_3.addWidget(input_url)
+        box_3.addStretch(1)
+
+        box_3.addWidget(manual_label)
+        box_3.addStretch(1)
 
         box_4 = QHBoxLayout()
-
-        box_4.addStretch(1)
-        box_4.addStretch(1)
+        box_4.addWidget(QLabel("URL"))
+        box_4.addWidget(self.input_url)
+        # box_4.addWidget(save_btn)
 
         box_5 = QHBoxLayout()
-        box_5.addStretch(1)
-        box_5.addWidget(QLabel("사용법 : 저장하고 싶은 YouTube 동영상의 URL을 입력하여 주세요"))
-        box_5.addStretch(1)
+        box_5.addWidget(save_btn)
+
         # 그리드 레이아웃 생성
         grid = QGridLayout()
-        grid.addWidget(save_btn, 0, 0)
+        grid.addWidget(set_save_btn, 0, 0)
         grid.addWidget(open_btn, 0, 1)
         grid.addWidget(reset_str_btn, 0, 2)
         grid.addWidget(browser_btn, 0, 3)
 
         vbox = QVBoxLayout()
+        vbox.addStretch()
         vbox.addLayout(box_1)
+        vbox.addStretch()
         vbox.addLayout(box_2)
-        vbox.addLayout(box_4)
+        vbox.addStretch()
         vbox.addLayout(box_3)
+        vbox.addStretch()
+        vbox.addLayout(box_4)
         vbox.addLayout(box_5)
+        vbox.addStretch()
         vbox.addLayout(grid)
+        vbox.addStretch()
 
         self.setLayout(vbox)
         self.setWindowTitle('YouTube Downloader')  # 프로그램 제목 설정
